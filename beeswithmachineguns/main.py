5512494f238924c8897a5d96013e4843d3a05331
Chris Kolosiwsky <ckolos@gmail.com>
2015-12-17 14:14:39 -0600

    cleanup spacing and format to make pep8 shut up; add option to pass AMIID as env var for Docker use

--- beeswithmachineguns/main.py
+++ beeswithmachineguns/main.py
@@ -32,6 +32,7 @@ from optparse import OptionParser, OptionGroup
 
 NO_TRAILING_SLASH_REGEX = re.compile(r'^.*?\.\w+$')
 
+
 def parse_options():
     """
     Handle the command line arguments for spinning up bees
@@ -53,13 +54,12 @@ commands:
     """)
 
     up_group = OptionGroup(parser, "up",
-        """In order to spin up new servers you will need to specify at least the -k command, which is the name of the EC2 keypair to use for creating and connecting to the new servers. The bees will expect to find a .pem file with this name in ~/.ssh/.""")
+                           """In order to spin up new servers you will need to specify at least the -k command, which is the name of the EC2 keypair to use for creating and connecting to the new servers. The bees will expect to find a .pem file with this name in ~/.ssh/.""")
 
     # Required
     up_group.add_option('-k', '--key',  metavar="KEY",  nargs=1,
                         action='store', dest='key', type='string',
                         help="The ssh key pair name to use to connect to the new servers.")
-
     up_group.add_option('-s', '--servers', metavar="SERVERS", nargs=1,
                         action='store', dest='servers', type='int', default=5,
                         help="The number of servers to start (default: 5).")
@@ -82,22 +82,21 @@ commands:
     parser.add_option_group(up_group)
 
     attack_group = OptionGroup(parser, "attack",
-            """Beginning an attack requires that you specify the URL(s) you wish to target, either using -u or -f.""")
+                               """Beginning an attack requires that you specify the URL(s) you wish to target, either using -u or -f.""")
 
     # Required
     attack_group.add_option('-u', '--url', metavar="URL", nargs=1,
-                        action='store', dest='url', type='string',
-                        help="URL of the target to attack.")
+                            action='store', dest='url', type='string',
+                            help="URL of the target to attack.")
     attack_group.add_option('-f', '--url-file', metavar="URL_FILE", nargs=1,
-                        action='store', dest='url_file', type='string',
-                        help="file containing URLs of the targets to attack.")
-
+                            action='store', dest='url_file', type='string',
+                            help="file containing URLs of the targets to attack.")
     attack_group.add_option('-n', '--number', metavar="NUMBER", nargs=1,
-                        action='store', dest='number', type='int', default=1000,
-                        help="The number of total connections to make to the target (default: 1000).")
+                            action='store', dest='number', type='int', default=1000,
+                            help="The number of total connections to make to the target (default: 1000).")
     attack_group.add_option('-c', '--concurrent', metavar="CONCURRENT", nargs=1,
-                        action='store', dest='concurrent', type='int', default=100,
-                        help="The number of concurrent connections to make to the target (default: 100).")
+                            action='store', dest='concurrent', type='int', default=100,
+                            help="The number of concurrent connections to make to the target (default: 100).")
     attack_group.add_option('--keepalive', metavar="KEEPALIVE",
                             action='store_true', dest='keepalive', default=False,
                             help='Whether or not to use ab keepalive (default: False)')
@@ -116,11 +115,11 @@ commands:
     output_group = OptionGroup(parser, "output")
 
     output_group.add_option('-o', '--output', metavar="OUTPUT_TYPE", nargs=1,
-                        action='store', dest='output_type', type='string',
-                        help="specify \'csv\' to output a csv row.  specify \'csvh\' to output csv headers prior to the csv row.")
+                            action='store', dest='output_type', type='string',
+                            help="specify \'csv\' to output a csv row.  specify \'csvh\' to output csv headers prior to the csv row.")
     output_group.add_option('-v', '--verbose', metavar="VERBOSE",
-                        action='store_true', dest='verbose', default=False,
-                        help="whether to log verbosely to stderr.")
+                            action='store_true', dest='verbose', default=False,
+                            help="whether to log verbosely to stderr.")
 
     parser.add_option_group(output_group)
 
@@ -131,25 +130,22 @@ commands:
 
     command = args[0]
 
+    AMIID = os.getenv('AMIID')
+    if AMIID:
+        options.instance = AMIID
+
     import logging
     if options.verbose:
-        level=logging.DEBUG
+        level = logging.DEBUG
     else:
-        level=logging.INFO
-
+        level = logging.INFO
     logging.basicConfig(level=level)
-#    loggingobject = logging.getLogger()
-#    print "Logging at", loggingobject.getEffectiveLevel()
+
     if command == 'up':
         if not options.key:
             parser.error('To spin up new instances you need to specify a key-pair name with -k')
-
-        #if options.group == 'default':
-        #    print 'New bees will use the "default" EC2 security group. Please note that port 22 (SSH) is not normally open on this group. You will need to use to the EC2 tools to open it before you will be able to attack.'
-
         bees.up(options.servers, options.group, options.zone, options.instance, options.instance_type, options.login, options.key, options.keepalive)
     elif command == 'attack':
-
         url, url_file = None, None
         if options.url_file:
             if not options.url_file.startswith('s3://'):
@@ -165,8 +161,6 @@ commands:
                 url = options.url
         else:
             parser.error('To run an attack you need to specify either a url with -u or a file with -f.')
-
-
         bees.attack(url, url_file, options.number, options.concurrent, options.keepalive, options.output_type, options.engine, options.time)
     elif command == 'down':
         bees.down()
@@ -180,4 +174,3 @@ def main():
 
 if __name__=='__main__':
     main()
-
